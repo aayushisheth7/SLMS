52766b4d8d2e3a1a1254159ef5d8a75d40faaab7
Nishiket Bhatt <nishiket04@gamil.com>
2023-09-23 21:30:47 +0530

    fixed js and add backed support

--- src/php/login.php
+++ src/php/login.php
@@ -1,26 +1,27 @@
 <?php 
 include('dbconnect.php');
-
-$username='nishiket04';
-$password='Nishiket#04';
-
+$username=$_POST['username'];
+$password=$_POST['password'];
 $ins="SELECT * FROM `login` WHERE username like ('$username') and pass like ('$password')";
 $query = mysqli_query($conn, $ins) or die(mysqli_error($conn));
 $count=mysqli_num_rows($query);
 if($count >=1){
-    session_start();
-    $_SESSION['username']=$usernmae;
-    echo "login";
-?>
+    ?>
     <script>
-      alert('Login Successfully');
+        alert("login success!")
     </script>
-<?php
+    <?php
+    session_start();
+    $_SESSION['username']=$username;
     header("Location: ../../student_home.html");
 }
 else{
-    echo "fail";
+    ?>
+    <script>
+        alert("Wrong username ans password");
+    </script>
+    <?php
+    // header('Location: ../../index.html');
 }
-
 ?>
 
