4fd6f42caa92fbe6530945744f4d4bae29c385b8
aayushi shet <aayushinsheth@gmail.com>
2024-04-25 08:49:45 +0530

    minor change

--- src/php/facl_reg.php
+++ src/php/facl_reg.php
@@ -1,63 +1,41 @@
 <?php
 include('dbconnect.php');
-
-$collegeCode = $_POST['college code'];
-$firstName =  $_POST['fname'];
-$middleName =  $_POST['mname'];
-$lastName =  $_POST['lname'];
-$email =  $_POST['email'];
-$gender =  $_POST['gender'];
+$collegeCode = $_POST['college code']; // Adjust this if your form field name is different
+$firstName = $_POST['fname'];
+$middleName = $_POST['mname'];
+$lastName = $_POST['lname'];
+$email = $_POST['email'];
+$gender = $_POST['gender'];
 $mobileNumber = $_POST['mobileno'];
-$qualification =  $_POST['qualification'];
-$position =  $_POST['position'];
-$teachingExperience =  $_POST['experience'];
-$expertise =  $_POST['expertise'];
-$emergencyContactName =  $_POST['emergencycontactname'];
-$emergencyContactNumber =  $_POST['emergencycontactnumber'];
-$emergencyContactRelation =  $_POST['emergencycontactrelation'];
-$age =  $_POST['age'];
-$dateOfBirth =  $_POST['dob'];
-$address =  $_POST['address'];
+$qualification = $_POST['qualification'];
+$position = $_POST['position'];
+$teachingExperience = $_POST['experience'];
+$expertise = $_POST['expertise'];
+$emergencyContactName = $_POST['emergencycontactname'];
+$emergencyContactNumber = $_POST['emergencycontactnumber'];
+$emergencyContactRelation = $_POST['emergencycontactrelation'];
+$age = $_POST['age'];
+$dateOfBirth = $_POST['dob'];
+$address = $_POST['address'];
 
 // File upload
+$profileImage = basename($_FILES["profileImage"]["name"]);
 $targetDirectory = "uploads/";
-$targetFile = $targetDirectory . basename($_FILES["profileImage"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+$targetFile = $targetDirectory . $profileImage;
+move_uploaded_file($_FILES["profileImage"]["tmp_name"], $targetFile);
 
-// Check if file is an actual image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["profileImage"]["tmp_name"]);
-    if($check !== false) {
-        $uploadOk = 1;
-    } else {
-        $uploadOk = 0;
-    }
-}
+// Insert data into database
+$sql = "INSERT INTO `faculty_reg` (`college code`, `fname`, `mname`, `lname`, `email`, `gender`, `mobileno`, `qualification`, `position`, `experience`, `expertise`, `emergencycontactname`, `emergencycontactnumber`, `emergencycontactrelation`, `age`, `dob`, `address`, `profileImage`) 
+        VALUES ('$collegeCode','$firstName','$middleName','$lastName','$email','$gender','$mobileNumber','$qualification','$position','$teachingExperience','$expertise','$emergencyContactName','$emergencyContactNumber','$emergencyContactRelation','$age','$dateOfBirth','$address','$profileImage')";
 
-// Check file size and format
-if ($_FILES["profileImage"]["size"] > 500000 || !in_array($imageFileType, ["jpg", "png", "jpeg", "gif"])) {
-    $uploadOk = 0;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-} else {
-    if (move_uploaded_file($_FILES["profileImage"]["tmp_name"], $targetFile)) {
-        $sql = "INSERT INTO `faculty_reg`(`college code`, `fname`, `mname`, `lname`, `email`, `gender`, `mobileno`, `qualification`, `position`, `experience`, `expertise`, `emergencycontactname`, `emergencycontactnumber`, `emergencycontactrelation`, `age`, `dob`, `address`, `profileImage`) 
-                VALUES ('$collegeCode','$firstName','$middleName','$lastName','$email','$gender','$mobileNumber','$qualification','$position','$teachingExperience','$expertise','$emergencyContactName','$emergencyContactNumber','$emergencyContactRelation','$age','$dateOfBirth','$address','$targetFile')";
-    
-$insert=mysqli_query($conn,$sql);
-if ($insert) {
-    // Registration successful, redirect to student dashboard
+if ($conn->query($sql) === TRUE) {
+    // Redirect to faculty dashboard
     header("Location: faculty_home.php");
     exit();
-} else {  
+} else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
-    }
-}
-$conn->close();
 
+// Close connection
+$conn->close();
 ?>
