611315744fdf32e1706cb8bf260f723a27adb173
nishiket04 <nishiket04@gmail.com>
2024-06-25 17:37:59 +0530

    some minor change

--- src/php/university_res.php
+++ src/php/university_res.php
@@ -3,7 +3,7 @@ class UniversityResults {
     private $conn;
     private $table = "unires";
 
-    public $usename;
+    public $username;
     public $sem;
     public $backlog;
     public $spi;
@@ -15,29 +15,27 @@ class UniversityResults {
     }
 
     // Method to retrieve results for a specific user
-    public function getResults($usename) {
+    public function getResults($username) {
         $query = "SELECT * FROM `".$this->table."` WHERE usename = ?";
         $stmt = $this->conn->prepare($query);
-        $stmt->bind_param("s", $usename);
+        $stmt->bind_param("s", $username);
         $stmt->execute();
         $result = $stmt->get_result();
         return $result;
     }
 
     // Method to insert new result
-    public function setResults($usename, $sem, $backlog, $spi, $cpi, $cgpa) {
-        $query = "INSERT INTO `".$this->table."` (`usename`, `sem`, `backlog`, `spi`, `cpi`, `cgpa`) VALUES (?, ?, ?, ?, ?, ?)";
-        $stmt = $this->conn->prepare($query);
-        $stmt->bind_param("siiddd", $usename, $sem, $backlog, $spi, $cpi, $cgpa);
-        $result = $stmt->execute();
-        return $result;
+    public function setResults($username, $sem, $backlog, $spi, $cpi, $cgpa) {
+        $query = "INSERT INTO `".$this->table."` (`username`, `sem`, `backlog`, `spi`, `cpi`, `cgpa`) VALUES ('$username', '$sem', '$backlog', '$spi', '$cpi', '$cgpa')";
+        $insert=mysqli_query($this->conn,$query);
+        return $insert;
     }
 
     // Method to delete a result by id
     public function deleteResults($id) {
         $query = "DELETE FROM `".$this->table."` WHERE id = ?";
         $stmt = $this->conn->prepare($query);
-        $stmt->bind_param("i", $id);
+        $stmt->bind_param("s", $id);
         $result = $stmt->execute();
         return $result;
     }
